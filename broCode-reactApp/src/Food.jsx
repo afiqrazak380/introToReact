db315f32165f2f24ca8a271285b34cb3dc3c2677
AfiqRazak <afiqrazak380@gmail.com>
2023-11-09 09:53:10 +0000

    React tutorial for beginners

---
+++ broCode-reactApp/src/Food.jsx
@@ -0,0 +1,19 @@
+
+function Food(){
+
+  const food1 = "orange";
+  const food2 = "apple";
+  
+  return(
+
+    <ul>
+      <li>banana</li>
+      <li>{food1}</li>
+      <li>{food2.toUpperCase()}</li>
+    </ul>
+  )
+
+
+}
+
+export default Food
